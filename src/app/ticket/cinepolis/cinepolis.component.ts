334922fe89adf3e27f5d65bf590817e61ba5ade1
Giezy Prado <giezyramirez27@gmail.com>
2023-10-06 19:44:22 -0500

    CINEPOLIS _____ GEPR 06/07/2023

--- src/app/ticket/cinepolis/cinepolis.component.ts
+++ src/app/ticket/cinepolis/cinepolis.component.ts
@@ -5,7 +5,74 @@ import { Component } from '@angular/core';
   templateUrl: './cinepolis.component.html',
   styleUrls: ['./cinepolis.component.css']
 })
-export class CinepolisComponent {
 
+
+
+export class CinepolisComponent {
+  nombre: string="";
+  boletos: number = 0;
+  compradores: number = 0;
+  tarjeta: string = "";
+  total: number = 0;
+  mensajeCompra:  string = "";
+  boletosPermitidos: number = 0;
+
+
+  compraCinepolis(){
+    this.total = 0;
+    this.boletosPermitidos = this.boletos / this.compradores;
+
+    if (this.boletosPermitidos > 7) {
+      this.mensajeCompra = "SOLO SE PERMITEN COMO MÁXIMO 7 BOLETOS POR PERSONA, TONTO...";
+    }
+    else{
   
+      if(this.boletos <= 2){
+        if(this.tarjeta == "SI"){
+          this.total = (this.boletos * 12) * 0.9
+          this.mensajeCompra = "DESCUENTO --> EL TOTAL A PAGAR ES: $" + this.total + " PARA " + this.compradores + " PERSONAS";
+        }
+        else{
+          this.total = (this.boletos * 12)
+          this.mensajeCompra = "EL TOTAL A PAGAR ES: $" + this.total + " PARA " + this.compradores + " PERSONAS";
+        }
+      }
+
+      else if(this.boletos > 2 && this.boletos <= 5){
+        if(this.tarjeta == "SI"){
+          this.total = ((this.boletos * 12) * 0.9) * 0.9
+          this.mensajeCompra = "DESCUENTO --> EL TOTAL A PAGAR ES: $" + this.total + " PARA " + this.compradores + " PERSONAS";
+        }
+        else{
+          this.total = (this.boletos * 12) * 0.9
+          this.mensajeCompra = "EL TOTAL A PAGAR ES: $" + this.total + " PARA " + this.compradores + " PERSONAS";
+        }
+      }
+
+      else if(this.boletos > 5){
+        if(this.tarjeta == "SI"){
+          this.total = ((this.boletos * 12) * 0.85) * 0.9
+          this.mensajeCompra = "DESCUENTO --> EL TOTAL A PAGAR ES: $" + this.total + " PARA " + this.compradores + " PERSONAS";
+        }
+        else{
+          this.total = (this.boletos * 12) * 0.85
+          this.mensajeCompra = "EL TOTAL A PAGAR ES: $" + this.total + " PARA " + this.compradores + " PERSONAS";
+        }
+      }
+
+      if(this.boletos == 0){
+        this.mensajeCompra = "POR FAVOR, INGRESE UN VALOR...";
+      }
+    }
+  }
+
+  cancelar(){
+    this.nombre = "";
+    this.boletos = 0;
+    this.compradores = 0;
+    this.tarjeta = "";
+    this.total = 0;
+    this.mensajeCompra = "";
+    this.boletosPermitidos = 0;
+  }
 }
